3d571dacf0de51842e37f51bbbc32b7b1a15f290
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-11-09 07:28:25 +0000

    Import local files

---
+++ VowelCountApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ReactiveUI;
+using System.Windows.Input;
+using Avalonia.Controls;
+
+namespace VowelCountApp.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string Greeting => "Welcome to Avalonia!";
+    public object ProcessString { get; }
+#pragma warning restore CA1822 // Mark members as static
+    
+        ProcessString = ReactiveCommand.Create(() =>
+        {
+            String str1 = TextBoxString.Text;
+            int vcount=0, i, str1len = str1.Length;
+
+            for (i = 0; i < str1len; i++)
+            {
+                str2 = Mid(str1, i);
+                if (str2)
+                {
+                    vcount++;
+                }
+            }
+        });
+
+}
